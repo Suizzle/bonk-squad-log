3ca1aeb251215986ec788b83d2ea5d02073ef8c2
suizzle <andygolay@gmail.com>
2023-12-13 00:36:47 -0500

    Complete but buggy. Error: Invalid arguments: player not provided.

--- programs/bonk-squad-log/src/lib.rs
+++ programs/bonk-squad-log/src/lib.rs
@@ -6,10 +6,97 @@ declare_id!("APozxuLZW4crR9nTPzQJEUpWMLubeC3bBUWs7ewCwtPE");
 pub mod bonk_squad_log {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn add_player(
+        ctx: Context<AddPlayer>,
+        name: String,
+        squad: String,
+        score: u64,
+    ) -> Result<()> {
+        msg!("Player Account Created");
+        msg!("Name: {}", name);
+        msg!("Squad: {}", squad);
+        msg!("Score: {}", score);
+
+        let player = &mut ctx.accounts.player;
+        player.key = ctx.accounts.initializer.key();
+        player.name = name;
+        player.squad = squad;
+        player.score = score;
         Ok(())
     }
+
+    pub fn update_player(
+        ctx: Context<UpdatePlayer>,
+        name: String,
+        squad: String,
+    ) -> Result<()> {
+        msg!("Player Account Updated");
+        msg!("Name: {}", name);
+        msg!("Squad: {}", squad);
+
+        let player = &mut ctx.accounts.player;
+        player.squad = squad;
+        Ok(())
+    }
+
+    pub fn delete_player(_ctx: Context<DeletePlayer>, name: String) -> Result<()> {
+        msg!("Player account for {} deleted", name);
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name:String, squad:String)]
+pub struct AddPlayer<'info> {
+    #[account(
+        init,
+        seeds = [name.as_bytes(), initializer.key().as_ref()],
+        bump,
+        payer = initializer,
+        space = 8 + 32 + 1 + 4 + name.len() + 4 + squad.len()
+    )]
+    pub player: Account<'info, PlayerAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name:String, squad:String)]
+pub struct UpdatePlayer<'info> {
+    #[account(
+        mut,
+        seeds = [name.as_bytes(), initializer.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 1 + 4 + name.len() + 4 + squad.len(),
+        realloc::payer = initializer,
+        realloc::zero = true,
+    )]
+    pub player: Account<'info, PlayerAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct DeletePlayer<'info> {
+    #[account(
+        mut,
+        seeds=[name.as_bytes(), initializer.key().as_ref()],
+        bump,
+        close=initializer
+    )]
+    pub player: Account<'info, PlayerAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+#[account]
+pub struct PlayerAccountState {
+    pub key: Pubkey,
+    pub name: String,
+    pub squad: String,
+    pub score: u64
+}
--- tests/bonk-squad-log.ts
+++ tests/bonk-squad-log.ts
@@ -1,16 +1,59 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { BonkSquadLog } from "../target/types/bonk_squad_log";
+import { assert, expect } from "chai"
+import { BonkSquadLog } from "../target/types/bonk_squad_log"
+import { BN } from "bn.js";
 
 describe("bonk-squad-log", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
-  const program = anchor.workspace.BonkSquadLog as Program<BonkSquadLog>;
+  const program = anchor.workspace
+    .BonkSquadLog as anchor.Program<BonkSquadLog>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  const player = {
+    name: "Just a test player",
+    squad: "The Thunderbirds",
+    score: 5,
+  }
+
+  const [playerPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(player.name), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  )
+
+  it("Player is added", async () => {
+    // Add test here.
+    const tx = await program.methods
+    .addPlayer(player.name, player.squad, new anchor.BN(player.score))
+    .rpc()
+
+    const account = await program.account.playerAccountState.fetch(playerPda)
+    expect(player.name === account.name)
+    expect(player.squad === account.squad)
+    expect(player.score === account.score.toNumber())
+    expect(account.key === provider.wallet.publicKey)  
+  })
+
+  xit("Player is updated`", async () => {
+    // test goes here
+    const newSquad = "new";
+    const newScore = 4;
+
+    const tx = await program.methods
+      .updatePlayer(player.name, newSquad)
+      .rpc()
+
+    const account = await program.account.playerAccountState.fetch(playerPda)
+    expect(player.name === account.name)
+    expect(newSquad === account.squad)
+    expect(newScore === account.score.toNumber())
+    expect(account.key === provider.wallet.publicKey)
+  })
+
+  xit("Deletes a player", async () => {
+    const tx = await program.methods
+    .deletePlayer(player.name)
+    .rpc()
+  })
+})
